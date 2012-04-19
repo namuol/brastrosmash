1f96a69c7d6b036e767529fece6bb7ee67f30045
Louis Acresti <louis.acresti@gmail.com>
2012-04-19 19:31:33 -0400

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p deploy
+cp -r akihabara index.html style.css *.png deploy/.
+
+git checkout gh-pages
+mv deploy/* .
+git add akihabara index.html style.css *.png
+
+rm -rf deploy
+git commit -am 'auto-deploy'
+git push origin gh-pages
+
+git checkout master
